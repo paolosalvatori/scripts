467cce7e15eb6aa273b624046f3ece64f7f9cbb9
Paolo Salvatori <leprino@hotmail.com>
2024-10-10 10:34:18 +0200

    Add files via upload

---
+++ install-nginx-ingress-controller-with-modsecurity.sh
@@ -0,0 +1,91 @@
+# Install kubectl
+az aks install-cli --only-show-errors
+
+# Get AKS credentials
+az aks get-credentials \
+  --admin \
+  --name $clusterName \
+  --resource-group $resourceGroupName \
+  --subscription $subscriptionId \
+  --only-show-errors
+
+# Check if the cluster is private or not
+private=$(az aks show --name $clusterName \
+  --resource-group $resourceGroupName \
+  --subscription $subscriptionId \
+  --query apiServerAccessProfile.enablePrivateCluster \
+  --output tsv)
+
+# Install Helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 -o get_helm.sh -s
+chmod 700 get_helm.sh
+./get_helm.sh &>/dev/null
+
+# Add Helm repos
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo add jetstack https://charts.jetstack.io
+
+# Update Helm repos
+helm repo update
+
+# Install Prometheus
+helm install prometheus prometheus-community/kube-prometheus-stack \
+  --create-namespace \
+  --namespace prometheus \
+  --set prometheus.prometheusSpec.podMonitorSelectorNilUsesHelmValues=false \
+  --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false
+
+# Install NGINX ingress controller using the internal load balancer
+helm install nginx-ingress ingress-nginx/ingress-nginx \
+  --create-namespace \
+  --namespace ingress-basic \
+  --set controller.replicaCount=3 \
+  --set controller.nodeSelector."kubernetes\.io/os"=linux \
+  --set defaultBackend.nodeSelector."kubernetes\.io/os"=linux \
+  --set controller.metrics.enabled=true \
+  --set controller.metrics.serviceMonitor.enabled=true \
+  --set controller.metrics.serviceMonitor.additionalLabels.release="prometheus" \
+  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz \
+  --set controller.config.modsecurity-snippet=\
+'SecRuleEngine On
+SecRequestBodyAccess On
+SecAuditLog /dev/stdout
+SecAuditLogFormat JSON
+SecAuditEngine RelevantOnly
+SecRule REMOTE_ADDR "@ipMatch 127.0.0.1" "id:87,phase:1,pass,nolog,ctl:ruleEngine=Off"' \
+
+# Install certificate manager
+helm install cert-manager jetstack/cert-manager \
+  --create-namespace \
+  --namespace cert-manager \
+  --set installCRDs=true \
+  --set nodeSelector."kubernetes\.io/os"=linux
+
+# Create cluster issuer
+cat <<EOF | kubectl apply -f -
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-nginx
+spec:
+  acme:
+    server: https://acme-v02.api.letsencrypt.org/directory
+    email: $email
+    privateKeySecretRef:
+      name: letsencrypt
+    solvers:
+    - http01:
+        ingress:
+          class: nginx
+          podTemplate:
+            spec:
+              nodeSelector:
+                "kubernetes.io/os": linux
+EOF
+
+# Create output as JSON file
+echo '{}' |
+  jq --arg x 'prometheus' '.prometheus=$x' |
+  jq --arg x 'cert-manager' '.certManager=$x' |
+  jq --arg x 'ingress-basic' '.nginxIngressController=$x' >$AZ_SCRIPTS_OUTPUT_PATH
